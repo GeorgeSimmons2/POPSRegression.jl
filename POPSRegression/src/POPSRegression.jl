780ee0ae435a6d411a8d8261eaf84e7e935918ec
George Simmons <GeorgeIsaacSimmons@outlook.com>
2025-06-09 12:03:14 +0100

    Had a problem with composing the covariance matrix in the function sigma. Now matches Tom's code

--- POPSRegression/src/POPSRegression.jl
+++ POPSRegression/src/POPSRegression.jl
@@ -1,14 +1,18 @@
 using LinearAlgebra
 using Random
 using Statistics
+using Plots
 
 function sigma(X; lambda_ = 1.0, alpha_ = 1.0)
     decomp = svd(X; full = false)
-    Vh = decomp.Vt
+    Vh = decomp.Vt                        # (n_components × n_features)
     eigen_vals_ = decomp.S .^ 2
-    divisor = vec(eigen_vals_ .+ lambda_ / alpha_)
-    divisor = reshape(divisor, 1, length(divisor))
-    scaled_sigma_ = Vh * transpose(Vh) ./ divisor
+    divisor = eigen_vals_ .+ lambda_ / alpha_
+
+    # divide each row of Vh by corresponding element of divisor
+    Vh_scaled = Vh ./ divisor[:, ones(Int, size(Vh, 2))]  # row-wise division
+
+    scaled_sigma_ = Vh' * Vh_scaled                        # (n_features × n_features)
     return scaled_sigma_
 end
 
@@ -24,33 +28,41 @@ function corrections(X, Y, scaled_sigma_, coeffs; leverage_percentile=0.0)
 end
 
 function hypercube(pointwise_corrections; percentile_clipping = 0.0)
-    eigvals, eigvecs = eigen(Symmetric(pointwise_corrections' * pointwise_corrections))
+    eig = eigen(Symmetric(pointwise_corrections' * pointwise_corrections))
+    eigvals = eig.values
+    eigvecs = eig.vectors
 
     mask = eigvals .> maximum(eigvals) * 1e-8
     eigvecs = eigvecs[:, mask]
     eigvals = eigvals[mask]
-    projections = deepcopy(eigvecs)
+
+    projections = eigvecs
     projected = pointwise_corrections * projections
 
-    lower = mapslices(col -> quantile(col, percentile_clipping / 100), projected; dims=1)
-    upper = mapslices(col -> quantile(col, 1.0 - percentile_clipping / 100), projected; dims=1)
-    
-    bounds = vcat(lower, upper)
+    lower = [quantile(projected[:, j], percentile_clipping / 100) for j in 1:size(projected, 2)]
+    upper = [quantile(projected[:, j], 1.0 - percentile_clipping / 100) for j in 1:size(projected, 2)]
+
+    bounds = vcat(lower', upper')  # (2 x N)
 
     return eigvecs, bounds
 end
 
-function sample_hypercube(projections, bounds, coeffs, number_of_committee_members = 10)
+function sample_hypercube(projections, bounds, coeffs; number_of_committee_members = 10)
     lower, upper = bounds[1, :], bounds[2, :]
 
     δθ = zeros((number_of_committee_members, size(projections, 1)))
     U = rand(Float64, (number_of_committee_members, size(lower, 1)))
-    committee = deepcopy(δθ)'
+    committee = zeros((number_of_committee_members, size(projections, 1)))'
     for j = 1:number_of_committee_members
         θ = projections * (((upper .- lower ) .* U[j, :] ) .+ lower)
         δθ[j, :] = θ
         committee[:, j] = θ .+ coeffs
     end
 
-    return committee, (δθ' * δθ) ./ size(δθ, 1)  
+    committee = projections * (lower[:, :]' .+ (upper .- lower)[:,:]' .* U)'
+    δθ        = committee * committee' ./ size(committee, 2)
+
+    committee = coeffs[:,:] .+ committee
+
+    return committee, δθ  
 end
