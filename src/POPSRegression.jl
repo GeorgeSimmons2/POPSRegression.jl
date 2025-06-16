0fc9276be2c97f1a679bd4a1167070f5aff76a41
GeorgeSimmons2 <GeorgeIsaacSimmons@outlook.com>
2025-06-16 16:06:40 +0100

    Formatting the package

--- src/POPSRegression.jl
+++ src/POPSRegression.jl
@@ -1,5 +1,49 @@
 module POPSRegression
 
-# Write your package code here.
+function corrections(X, Y, Gamma; leverage_percentile=0.0)
+    C      = (Gamma' * Gamma ./ size(X,1) .+ X' * X)
+    A      = C \ X'
+    leverage = diag(X * A)
+    coeffs = C \ (X' * Y)
+    errors = Y .- (X * coeffs)
+    leverage_threshold = quantile(leverage, leverage_percentile)
+    mask = leverage .>= leverage_threshold
+    pointwise_corrections = A[:,mask]'
+    pointwise_corrections = pointwise_corrections .* (errors[mask] ./ leverage[mask])
+    return pointwise_corrections
+end
+
+function hypercube(pointwise_corrections; percentile_clipping = 0.0)
+    eig = eigen(Symmetric(pointwise_corrections' * pointwise_corrections))
+    eigvals = eig.values
+    eigvecs = eig.vectors
+
+    mask = eigvals .> maximum(eigvals) * 1e-8
+    eigvecs = eigvecs[:, mask]
+    eigvals = eigvals[mask]
+
+    projections = eigvecs
+    projected = pointwise_corrections * projections
+
+    lower = [quantile(projected[:, j], percentile_clipping / 100) for j in 1:size(projected, 2)]
+    upper = [quantile(projected[:, j], 1.0 - percentile_clipping / 100) for j in 1:size(projected, 2)]
+
+    bounds = vcat(lower', upper')  # (2 x N)
+
+    return eigvecs, bounds
+end
+
+function sample_hypercube(projections, bounds, coeffs; number_of_committee_members = 50)
+    lower, upper = bounds[1, :], bounds[2, :]
+
+    U = rand(Float64, (number_of_committee_members, size(lower, 1)))
+
+    committee = projections * (lower[:, :]' .+ (upper .- lower)[:,:]' .* U)'
+    δθ        = committee * committee' ./ size(committee, 2)
+
+    committee = coeffs[:,:] .+ committee
+    
+    return committee, δθ  
+end
 
 end
